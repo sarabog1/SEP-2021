74c3edbbee04b25627bdc0c28e8c782b48485a02
Sara Bogunovic <sara.bogunovic.17@singimail.rs>
2021-05-31 13:39:41 +0200

    init a new react project

---
+++ 04-front-end/src/components/application/Application.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Application.css';
+
+function Application() {
+  return (
+    <div className="App">
+      ...
+    </div>
+  );
+}
+
+export default Application;
---
+++ 04-front-end/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Application from './components/application/Application';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
